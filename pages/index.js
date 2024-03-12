7be2093189a4b14720c496617ff7a86ce6ac61db
monkepi18 <patelpiyush1810@gmail.com>
2024-03-12 14:52:31 +0530

    updated soon

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import HomeMain from '../components/HomeMain'
 import HomeAlert from '../components/HomeAlert'
-import CenteredBlockPage1 from '../components/HomeTemp'
+import CenteredBlockPage from './tuned'
 
 // sqrt(sqr(mag(S11+S12*(re(x2)+im(y2)))+sqr(mag(S21+S22*re(x2)+im(y2)))))/2
 
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="description" content="MAC-2024" />
         <link rel="icon" href="/icon2.png" />
       </Head> */}
-      <CenteredBlockPage1/>
+      <CenteredBlockPage/>
       {/* <HomeMain/> */}
       {/* <HomeAlert /> */}
     </div>
